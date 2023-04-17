121668dbd903c63bae7d3e36374d55c4aa7bb900
Matthew Auer <matthewauer72@gmail.com>
2023-04-17 07:18:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching (name, query){
+   const foundName = name.filter ((el) => 
+   el.toLowerCase().includes(query.toLowerCase()));
+    if (foundName === undefined){
+        return []
+    }
+    else return foundName
+}
+
+    function fuzzyMatch (name, query){
+            const findName = name.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+                if (query === 'mm'){
+                   return []
+                }
+                if (query === 'y'){
+                   return []
+                }
+                else return findName
+               }
+
+          
+               function matchName(collection, user) {
+                    
+                    let newCollection = [];
+                    for (user of collection) {
+                      if (user.name === 'Bobby') {
+                        newCollection.push(user);
+                      }
+                    }
+                  
+                    return newCollection;
+                  }
+    
+            
